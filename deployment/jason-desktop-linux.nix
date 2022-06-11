12bef1aa8212f3f12e30a87ead3677a6bd82db8b
Jason Yundt <jason@jasonyundt.email>
2022-06-11 11:11:06 -0400

    Set up Vulkan

--- deployment/jason-desktop-linux.nix
+++ deployment/jason-desktop-linux.nix
@@ -1,5 +1,6 @@
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
 # SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022)
+{ config, pkgs, ... }:
 {
 	imports =
 		[
@@ -12,6 +13,7 @@
 		hostName = "Jason-Desktop-Linux";
 		interfaces.enp8s0.useDHCP = true;
 	};
+	environment.systemPackages = [ pkgs.vulkan-tools ];
 	time.timeZone = "America/New_York";
         nixpkgs.config.allowUnfree = true;
         programs.steam.enable = true;
