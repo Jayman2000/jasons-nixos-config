b5256b1c3006cd2d3b563f65663e191c7ba188b4
Jason Yundt <jason@jasonyundt.email>
2022-06-13 13:50:23 -0400

    Fix git-tb not displaying a usage error
    Before this change, git-tb would only display an error if it was given
    arguments. For example, this would cause the error to appear:
    	git tb foo bar
    , but this wouldn’t:
    	git tb foo
    
    This change also makes the code (theoretically) more efficient and
    prevents the error from appearing multiple times.

--- deployment/applications/git-tb.nix
+++ deployment/applications/git-tb.nix
@@ -9,24 +9,28 @@ writeShellApplication {
 		subcommand="$1"
 		shift
 
-		for branch_name in "$@"; do
-			if [ "$subcommand" = create ]; then
+		if [ "$subcommand" = create ]; then
+			for branch_name in "$@"; do
 				git checkout -b "$branch_name" main &&
 				git push -u syncthing
-			elif [ "$subcommand" = merge ]; then
+			done
+		elif [ "$subcommand" = merge ]; then
+			for branch_name in "$@"; do
 				git checkout main &&
 				git merge --ff-only "$branch_name" &&
 				git push &&
 				git branch -d "$branch_name" &&
 				git push -d syncthing "$branch_name"
-			elif [ "$subcommand" = delete ]; then
+			done
+		elif [ "$subcommand" = delete ]; then
+			for branch_name in "$@"; do
 				git branch -D "$branch_name" &&
 				git push -d syncthing "$branch_name"
-			else
-				# shellcheck disable=SC1111
-				echo "Unknown subcommand: “$subcommand”"
-				exit 1
-			fi
-		done
+			done
+		else
+			# shellcheck disable=SC1111
+			echo "Unknown subcommand: “$subcommand”"
+			exit 1
+		fi
 	'';
 }
