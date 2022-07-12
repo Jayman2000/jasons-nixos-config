c2111d3f8a2b35867c13f05bf14203c0c7bc8e31
Jason Yundt <jason@jasonyundt.email>
2022-07-12 15:11:16 -0400

    Only sync with desktop on other devices

--- deployment/syncthing.nix
+++ deployment/syncthing.nix
@@ -1,5 +1,6 @@
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
 # SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022)
+{ config, ... }:
 {
 	services.syncthing = {
 		enable = true;
@@ -10,24 +11,38 @@
 		extraOptions.gui.tls = true;
 		overrideDevices = true;
 		overrideFolders = true;
-		devices = {
+
+		devices = let
+			desktop-device = {
+				"Jason-Desktop-Linux" = { id = "VQ3YVQK-FPN6REG-T3BNHX2-LT5YS4O-YFGVXPV-ENJM3AZ-LD7AE5W-FFS"; };
+			};
+		in
+		{
 			"Server" = { id = "QZBHFNE-XJWGGY4-6JXYMD3-D3HVGR2-C64BVH2-6M644XU-RSVRGAS-QZ752Q7"; };
 			"Jason-Lemur-Pro" = { id = "XDRFUGH-DWTEVBO-YELO3PG-2QWAFMS-D3H3NKH-CADVPI3-REOFSY5-NZDDXAE"; };
-			"Jason-Desktop-Linux" = { id = "VQ3YVQK-FPN6REG-T3BNHX2-LT5YS4O-YFGVXPV-ENJM3AZ-LD7AE5W-FFS5JQI"; };
 			"Jason-Desktop-Windows" = { id = "DAW6JNR-DHBHAVL-42UVJDB-SENEDDQ-OVLHNH3-XOVKDE4-JXVIQ23-GJBG6QZ"; };
-		};
-		folders = {
+		# In other words, only add Jason-Desktop-Linux to the devices list if this config isn’t being deployed to Jason-Desktop-Linux.
+		} // (if config.networking.hostName != "Jason-Desktop-Linux" then desktop-device else { });
+
+		folders = let
+			all-others = [
+				"Server"
+				"Jason-Lemur-Pro"
+				"Jason-Desktop-Windows"
+			] ++ (if config.networking.hostName != "Jason-Desktop-Linux" then [ "Jason-Desktop-Linux" ] else [ ]);
+		in
+		{
 			"Keep Across Linux Distros!" = {
 				id = "syrpl-vpqnk";
-				devices = [ "Server" "Jason-Lemur-Pro" "Jason-Desktop-Linux" "Jason-Desktop-Windows" ];
+				devices = all-others;
 			};
 			"Projects" = {
 				id = "mjwge-zeznc";
-				devices = [ "Server" "Jason-Lemur-Pro" "Jason-Desktop-Linux" "Jason-Desktop-Windows" ];
+				devices = all-others;
 			};
 			"Game Data" = {
 				id = "eheef-uq5hv";
-				devices = [ "Server" "Jason-Lemur-Pro" "Jason-Desktop-Linux" "Jason-Desktop-Windows" ];
+				devices = all-others;
 			};
 		};
 	};
