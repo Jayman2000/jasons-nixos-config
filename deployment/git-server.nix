3014d0c12c499d72e2f743f5afccfaf6d839c011
Jason Yundt <jason@jasonyundt.email>
2022-08-07 14:52:13 -0400

    Make git’s shell git-shell
    The only reason anyone should be SSHing as the git user is to use
    git-clone. git-shell makes that possible.

--- deployment/git-server.nix
+++ deployment/git-server.nix
@@ -1,12 +1,13 @@
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
 # SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022)
-{ config, ... }:
+{ config, pkgs, ... }:
 {
 	users.groups.git = { };
 	users.users.git = {
 		description = "Owner of Git repos. git-clone should use this user when cloning over SSH.";
 		group = "git";
 		isSystemUser = true;
+		shell = "${pkgs.git}/bin/git-shell";
 
 		# Git repos will be stored here. Also needed for the SSH
 		# authorized keys file.
