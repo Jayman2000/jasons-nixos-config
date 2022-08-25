3175e7dc9f073a75d702ab7d619b4e461c21a834
Jason Yundt <jason@jasonyundt.email>
2022-08-25 11:40:26 -0400

    Automatically start swapspace
    This will (hopefully) allow me to put a ton of stuff into tmpfs folders
    without worrying about running virtual memory. I’m trying to build
    Ubuntu Touch for the F(x)tec Pro¹ X [1], and I’m running into a lot of
    issues. Its CI script [2] doesn’t use the -b flag. Not using the -b flag
    makes a bunch of files get downloaded to /run (which is a tmpfs). I
    think that things will work better if I don’t use the -b flag, but when
    I try that on my machines, I end up running out of space in /run.
    
    [1]: <https://gitlab.com/ubports/porting/community-ports/android11/fxtec-pro1x/fxtec-pro1x#how-to-build>
    [2]: <https://gitlab.com/ubports/porting/community-ports/android11/fxtec-pro1x/fxtec-pro1x/-/blob/7193487bb6d9a9d7ded5df53d499e5b58f213d80/.gitlab-ci.yml#L11>

--- deployment/tmpfs-and-swap.nix
+++ deployment/tmpfs-and-swap.nix
@@ -1,10 +1,45 @@
-# SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
+# SPDX-FileNotice: 🅭🄍1.0 Unless otherwise noted, everything in this file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
+# SPDX-License-Identifier: GPL-2.0-or-later
 # SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022)
+# SPDX-FileContributor: Jacob Adams <tookmund@gmail.com>
 { config, pkgs, ... }:
 {
-	environment.systemPackages = [ (import ./applications/swapspace.nix) ];
 	boot.tmpOnTmpfs = true;
 	# This is probably way more than I need, but I have a lot of swap, so
 	# it’s probably fine.
 	boot.tmpOnTmpfsSize = "290%";
+
+	# This next section was adapted from Swapspace’s swapspace.service. See:
+	# <https://github.com/Tookmund/Swapspace/blob/62c25dbc3f4741f23c99b6c9310c17d63391ad10/swapspace.service>
+	# BEGIN GPL-2.0-or-later LICENSED SECTION
+	systemd.services.swapspace = let
+		dependencies = [ "local-fs.target" "swap.target" ];
+		swapFileDirBase = if config.networking.hostName == "Jason-Desktop-Linux" then "/hdd/home" else "";
+		swapFileDir = "${swapFileDirBase}/var/lib/swapspace";
+		swapspacePkg = (import ./applications/swapspace.nix);
+	in {
+		description = "Swapspace, a dynamic swap space manager";
+		documentation = [ "man:swapspace(8)" ];
+		after = dependencies;
+		requires = dependencies;
+
+		path = [
+			pkgs.coreutils  # for mkdir and chmod
+			pkgs.util-linux  # for mkswap (swapspace runs it)
+			swapspacePkg
+		];
+		# Technically, I could use mkdir’s -m flag instead of chmod,
+		# but then (I’m assuming) /var and /var/lib would have their
+		# permissions set to 700 if they didn’t already exist.
+		script = ''
+			[ -d '${swapFileDir}' ] && mkdir -p '${swapFileDir}' && chmod 700 '${swapFileDir}'
+			swapspace --swappath='${swapFileDir}'
+		'';
+		serviceConfig = {
+			Restart = "always";
+			RestartSec = 30;
+		};
+		wantedBy = [ "multi-user.target" ];
+	};
+	# END GPL-2.0-or-later LICENSED SECTION
 }
