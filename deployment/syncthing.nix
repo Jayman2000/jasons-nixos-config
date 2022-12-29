a29c0f97bc29e95aaadb92ab03dec6a2416a484a
Jason Yundt <jason@jasonyundt.email>
2022-12-28 19:35:52 -0500

    Update Graphical-Test-VM’s Syncthing ID
    The last commit, 0aaa8df (Instruct users to check HM version up front,
    2022-12-25), changed the installation instructions for this config. In
    order to test those new instructions, I reinstalled Graphical-Test-VM.
    When you reinstall a system, a new Syncthing ID is generated. This
    commit updates Graphical-Test-VM’s Syncthing ID to match the one that
    was generated with this new installation.

--- deployment/syncthing.nix
+++ deployment/syncthing.nix
@@ -17,7 +17,7 @@
 				"Jason-Desktop-Linux" = { id = "7A735CO-FSRRF2I-FN5WRGV-OHGRWHR-TF4Z47H-OJBHRBA-G7CP7BN-FTLXGAX"; };
 			};
 			graphical-test-vm-device = {
-				"Graphical-Test-VM" = { id = "TGH2DYG-CR3KY4I-T77EEFI-OZU4WDH-TA7TMS4-F6KKBLH-ZNYXHOE-32DDJAT"; };
+				"Graphical-Test-VM" = { id = "BPBRJH4-LTZKLKE-Z7TJLGV-DOLKX5X-PFILLHG-NC7MTZD-PX3TMZC-QUOQTAX"; };
 			};
 			laptop-device = {
 				"Jason-Laptop-Linux" = { id = "HIUQOJU-CNAGZCU-BHAFKP7-2T4WAO3-XUMWZKC-N2ZXQWD-XSGWNZH-WRGEWAP"; };
