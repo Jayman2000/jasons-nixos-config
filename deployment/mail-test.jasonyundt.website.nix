36f0d02022b1ce2e90e8ce630ca5765713a8bee1
Jason Yundt <jason@jasonyundt.email>
2023-02-04 16:15:53 -0500

    Create initial config for mail-test.jasonyundt.website
    This will be my test mail server. My production mail server will stay on
    jasonyundt.email. Normally, I would put this on a subdomain of
    jasonyundt.email (for example, test.jasonyundt.email), but Mail-in-a-Box
    comes with its own DNS server, and I don’t know if Mail-in-a-Box allows
    me to add arbitrary DNS records.
    
    Once I get the test server working well, I’m going to transition
    jasonyundt.email to NixOS.

---
+++ deployment/mail-test.jasonyundt.website.nix
@@ -0,0 +1,23 @@
+# SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
+# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022–2023)
+{ config, pkgs, ... }:
+{
+	imports = [
+		./home-manager/22.11.nix
+		./common.nix
+		./auto-upgrade.nix
+	];
+
+	# The server itself is in Paris, but I’ll be using it from
+	# computers in America, so no timezone quite makes sense here.
+	time.timeZone = "Etc/UTC";
+
+	# The goal here is to make networking.fqdn accurate.
+	networking.hostName = "mail-test";
+	networking.domain = "jasonyundt.website";
+
+	boot.initrd.network.ssh.enable = true;
+	users.users.root.openssh.authorizedKeys.keys = [
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGOkLREBd8ijpssLjYJABnPiAEK11+uTkalt1qO3UntX jayman@Jason-Desktop-Linux"
+	];
+}
