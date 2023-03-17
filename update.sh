3b8b0b78f4e428c622d9c042817a0e18e8735f5d
Jason Yundt <jason@jasonyundt.email>
2023-03-17 12:26:37 -0400

    Prevent update.sh from prompting the user
    Sometimes, you know ahead of time that rebuilding the NixOS
    configuration is going to take a while. It’s kind of annoying to walk
    away from the machine because it’s going to take a while and then to
    come back to see that it’s waiting for you to answer a question from
    flatpak.
    
    Implements <mid:20221223123639.53dhb5xnanwigl5g@Jason-Laptop-Linux>.

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
 # SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022–2023)
 
-sudo nixos-rebuild boot --upgrade && flatpak update
+sudo nixos-rebuild boot --upgrade && flatpak update --noninteractive
