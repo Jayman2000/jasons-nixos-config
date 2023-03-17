e978a05c7cc87d89179d2c2888d0fc42c367a6a6
Jason Yundt <jason@jasonyundt.email>
2023-03-17 09:10:04 -0400

    Make Ctrl+C stop shell scripts
    Before this change, if you ran deploy.sh or update.sh and wanted to stop
    them, then you would have to hit Ctrl+C multiple times.
    
    Implements <mid:20220605012509.ms66zmmoasbolu6v@Jason-Laptop-Linux>.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
-# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022)
+# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022–2023)
+set -e
 
 function copy_and_restrict {
 	# Usage: copy_and_restrict <mode> <file> [file]…
