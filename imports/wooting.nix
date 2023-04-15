e2238d3d7391e837aa519883db1faae46c5a3e34
Jason Yundt <jason@jasonyundt.email>
2023-04-15 16:04:38 -0400

    Update Wootility
    Implements <mid:20230415175426.gtfaukfrb26f3h4h@Jason-Desktop-Linux>.

--- imports/wooting.nix
+++ imports/wooting.nix
@@ -2,13 +2,29 @@
 # SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2023)
 {
 	nixpkgs.overlays = let
-		# This PR (<https://github.com/NixOS/nixpkgs/pull/225055>) adds
-		# support for the Wooting Two HE.
-		url = "https://github.com/jtrees/nixpkgs/archive/refs/heads/update-wooting-udev-rules.tar.gz";
-		tarball = builtins.fetchTarball url;
-		prNixpkgs = import tarball {};
+		# <https://github.com/NixOS/nixpkgs/pull/225055>
+		updatedUdevPkgs = let
+			url = "https://github.com/jtrees/nixpkgs/archive/refs/heads/update-wooting-udev-rules.tar.gz";
+			tarball = builtins.fetchTarball url;
+		in import tarball {};
+		# <https://github.com/NixOS/nixpkgs/pull/225053>
+		updatedWootilityPkgs = let
+			url = "https://github.com/jtrees/nixpkgs/archive/refs/heads/update-wootility.tar.gz";
+			tarball =  builtins.fetchTarball url;
+		in import tarball {};
+
+		# This overlay is only really needed since the Wooting
+		# Two HE is so new.
 		overlay = (old: new: {
-			wooting-udev-rules = prNixpkgs.wooting-udev-rules;
+			# We need an updated version of this package in
+			# order for there to be udev rules for the
+			# Wooting Two HE.
+			wooting-udev-rules = updatedUdevPkgs.wooting-udev-rules;
+			# We need an updated version of this package
+			# because the version that’s currently in
+			# Nixpkgs is too old to support the Wooting Two
+			# HE.
+			wootility = updatedWootilityPkgs.wootility;
 		});
 	in [ overlay ];
 
