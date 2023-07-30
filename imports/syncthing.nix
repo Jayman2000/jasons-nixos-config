b748bc848be8b75aa62538182470e49f911a0d3e
Jason Yundt <jason@jasonyundt.email>
2023-07-30 10:32:44 -0400

    Update Jason-Lemur-Pro-VM-Test’s Syncthing ID
    While I was testing out a commit in the jasons-rhel-config repo [1], I
    ended up reinstalling the OS on Jason-Lemur-Pro and
    Jason-Lemur-Pro-VM-Test a lot. As a result, Jason-Lemur-Prom-VM-Test now
    has a new Syncthing ID.
    
    Fixes <mid:qog3fceaoccp6xs6snyzz6jiuvn4t3uarkqtlxasm5j3xm5iop@344jdgv6bz26>.
    
    [1]: <https://jasonyundt.website/gitweb?p=jasons-rhel-config;a=summary>

--- imports/syncthing.nix
+++ imports/syncthing.nix
@@ -26,7 +26,7 @@
 		{
 			"Server" = { id = "QZBHFNE-XJWGGY4-6JXYMD3-D3HVGR2-C64BVH2-6M644XU-RSVRGAS-QZ752Q7"; };
 			"Jason-Lemur-Pro" = { id = "3PGXPGR-OWRBYZG-JQEQH73-XE7LIUP-F6OEYWJ-IC3DLCW-WHNAU3R-G2P6SAM"; };
-			"Jason-Lemur-Pro-VM-Test" = { id = "MRLHROA-JB6ZV7Y-QQORSLQ-SQ5DOBW-3DCCF6Q-C4VWYKL-UFI7EDD-B6THXQB"; };
+			"Jason-Lemur-Pro-VM-Test" = { id = "2MOLIOF-XEWO4JR-PUE4NUS-I3YSRGM-X374W7F-6BXK4S6-UGXVIL6-TYWHWAC"; };
 			"Jason-Desktop-Windows" = { id = "DAW6JNR-DHBHAVL-42UVJDB-SENEDDQ-OVLHNH3-XOVKDE4-JXVIQ23-GJBG6QZ"; };
 		# In other words, only add each device to the devices list if this config isn’t being deployed on that device.
 		} // (if config.networking.hostName != "Jason-Desktop-Linux" then desktop-device else { })
