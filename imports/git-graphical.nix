5a08228ea5f998313213ef7ab876e2f7aa31a30c
Jason Yundt <jason@jasonyundt.email>
2023-08-11 20:25:32 -0400

    [WIP] Remove stuff that was only needed for NixOS 22.11
    TODO:
    • Finish writing commit message.
    • Test on:
    	◦ mailserver.test.jasonyundt.website
    	◦ Jason-Desktop-Linux
    	◦ Jason-Laptop-Linux
    	◦ Graphical-Test-VM
    	◦ jasonyundt.website.home.arpa

--- imports/git-graphical.nix
+++ imports/git-graphical.nix
@@ -10,41 +10,7 @@
 	home-manager.users.jayman = { pkgs, ... }: {
 		# Adapted from
 		# <https://nix-community.github.io/home-manager/index.html#_how_do_i_install_packages_from_nixpkgs_unstable>.
-		home.packages = let
-			unstablePkgs = (import ./nixpkgs-unstable.nix);
-			# Thanks to strager
-			# (<https://stackoverflow.com/users/39992/strager>)
-			# for this idea:
-			# <https://stackoverflow.com/a/71245733/7593853>
-			pyOverrides = finalAttrs: previousAttrs: {
-				# The version of python3Packages.cffi in
-				# the 22.11 version of nixpkgs doesn’t
-				# work with Python 3.11. See
-				# <https://foss.heptapod.net/pypy/cffi/-/issues/551>.
-				#
-				# The only reason we want cffi to work
-				# with Python 3.11 it’s an indirect
-				# dependency of pre-commit. See below
-				# for why we want to run pre-commit with
-				# Python 3.11.
-				cffi = unstablePkgs.python311Packages.cffi;
-			};
-			overlay = finalAttrs: previousAttrs: {
-				python311 = previousAttrs.python311.override {
-					packageOverrides = pyOverrides;
-				};
-			};
-			pkgsToUse = (
-				# See the comment in the pyOverrides
-				# declaration.
-				if config.system.nixos.release == "22.11" then
-					import <nixpkgs> {
-						overlays = [ overlay ];
-					}
-				else
-					pkgs
-			);
-		in [
+		home.packages = [
 			pkgsToUse.cargo # Used for this repo’s pre-commit config
 			pkgsToUse.gcc # Used for this repo’s pre-commit config
 			pkgsToUse.go # Used for this repo’s pre-commit config
