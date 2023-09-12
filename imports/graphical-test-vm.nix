0d79100dd5d6757e48ed78424994d3aa21e10580
Jason Yundt <jason@jasonyundt.email>
2023-09-12 19:47:39 -0400

    Make VMs use serial consoles
    Serial consoles allow you to copy and paste. Hopefully, being able to
    copy and paste into VMs will save me some time.
    
    Implements <mid:2dm2wu2akjfqcq2aivnpyiur36ozqftjpqwfpk6p2tcbqzuzai@kuxla43wc3gd>.

--- imports/graphical-test-vm.nix
+++ imports/graphical-test-vm.nix
@@ -1,5 +1,5 @@
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
-# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022)
+# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022–2023)
 {
 	imports =
 		[
@@ -8,6 +8,7 @@
 			./efi.nix
 			./cli-shortcuts.nix
 			./graphical.nix
+			./serial-console.nix
 		];
 	networking.hostName = "Graphical-Test-VM";
 	time.timeZone = "America/New_York";
--- imports/jasonyundt.website.home.arpa.nix
+++ imports/jasonyundt.website.home.arpa.nix
@@ -1,10 +1,13 @@
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
-# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022)
+# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022–2023)
 #
 # This file is used to test jasonyundt.website in a local VM. That’s why its host name ends in .home.arpa (see <https://www.ctrl.blog/entry/homenet-domain-name.html>).
 { config, pkgs, ... }:
 {
-	imports = [ ./jasonyundt.website-common.nix ];
+	imports = [
+		./jasonyundt.website-common.nix
+		./serial-console.nix
+	];
 
 	# The goal here is to make networking.fqdn accurate.
 	networking.domain = "website.home.arpa";
