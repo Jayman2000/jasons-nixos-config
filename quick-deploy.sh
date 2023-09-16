7587bc575e2c365d8049a00b4d09481f088632dc
Jason Yundt <jason@jasonyundt.email>
2023-09-16 10:33:09 -0400

    Allow quick-deploy.sh to be run without being sourced
    At one point in time, I had envisioned running nicely-stop-session like
    this:
    
    	{ nohup nicely-stop-session reboot & } && disown && exit
    
    I no longer think that it makes sense to run nicely-stop-session like
    this. See bfc0a34 (Remove 30 second delay from nicely-stop-session,
    2023-09-13).
    
    Before this change, you would have to run
    “source quick-deploy.sh <whatever>” in order to make the script work.
    The only reason why I wanted to make users run the script like this was
    to allow me to use nohup and exit the way that I outlined above.  Now
    that I no longer think that that makes sense, there’s no reason to run
    quick-deploy.sh that way.
    
    3c5fcd9 (Create quick-deploy.sh, 2023-09-13) should have never made the
    user source the script in the first place. By the time that commit was
    started, I had already decided to not use nohup and exit.
    
    Implements <mid:gcub2rgu6iawdj2engrfmifhvfl2n2i3g4mobfq7grnldfbhl7@xocnmbjaci3b>.

--- quick-deploy.sh
+++ quick-deploy.sh
@@ -1,44 +1,30 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p nix
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
 # SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2023)
+set -e
+# See sysexits.h.
+readonly ex_usage=64
 
-# Thanks mklement0 (<https://stackoverflow.com/users/45375/mklement0>)
-# for this answer: <https://stackoverflow.com/a/28776166/7593853>
-if (return 0 2>/dev/null)
+if [ "$#" -eq 1 ]
 then
-	if type nix-build > /dev/null
+	if [ "$1" = shutdown ] || [ "$1" = reboot ]
 	then
-		if [ "$#" -eq 1 ]
-		then
-			if [ "$1" = shutdown ] || [ "$1" = reboot ]
-			then
-				sudo -v &&
-					pushd imports/applications &&
-					nix-build nicely-stop-session.nix &&
-					popd &&
-					./deploy.sh &&
-					./imports/applications/result/bin/nicely-stop-session "$1"
-			else
-				echo -E \
-					"ERROR: Invalid session stop" \
-					"type  “$1”. It should either" \
-					"be “shutdown” or" \
-					"“reboot”." 1>&2
-			fi
-		else
-			echo -E \
-				"ERROR: 1 argument should have been" \
-				"given, but $# were given." 1>&2
-		fi
+		sudo -v
+		pushd imports/applications
+		nix-build nicely-stop-session.nix
+		popd
+		./deploy.sh
+		./imports/applications/result/bin/nicely-stop-session "$1"
 	else
-		echo \
-			ERROR: nix-build doesn’t appear to be on your \
-			PATH. 1>&2
+		echo -E \
+			"ERROR: Invalid session stop type  “$1”. It" \
+			"should either be “shutdown” or “reboot”." 1>&2
+		exit "$ex_usage"
 	fi
 else
 	echo -E \
-		"ERROR: You can’t run this script as an executable." \
-		"You have to run “source $0 (shutdown|reboot)”" 1>&2
-	# See sysexits.h
-	exit 64
+		"ERROR: 1 argument should have been given, but $#" \
+		"were given." 1>&2
+	exit "$ex_usage"
 fi
