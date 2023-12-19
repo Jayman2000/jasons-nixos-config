bded054faba1ba53062ebeab81285f12fc773ee1
Jason Yundt <jason@jasonyundt.email>
2023-12-19 14:26:35 -0500

    Get Wooting-related packages from stable Nixpkgs
    When I first got my Wooting Two HE, I wasn’t able to use the stable
    versions of the Wooting-related packages. The Wooting Two HE was so new,
    that those packages hadn’t been updated to support it yet. Now, the
    stable versions of those packages do support the Wooting Two HE, so
    there’s no real advantage to using the unstable versions.

--- imports/wooting.nix
+++ imports/wooting.nix
@@ -1,24 +1,6 @@
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
 # SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2023)
 {
-	nixpkgs.overlays = let
-		unstablePkgs = import ./nixpkgs-unstable.nix;
-
-		# This overlay is only really needed since the Wooting
-		# Two HE is so new.
-		overlay = (old: new: {
-			# We need an updated version of this package in
-			# order for there to be udev rules for the
-			# Wooting Two HE.
-			wooting-udev-rules = unstablePkgs.wooting-udev-rules;
-			# We need an updated version of this package
-			# because the version that’s currently in
-			# Nixpkgs is too old to support the Wooting Two
-			# HE.
-			wootility = unstablePkgs.wootility;
-		});
-	in [ overlay ];
-
 	hardware.wooting.enable = true;
 	# This is needed for the udev rules to work. See
 	# <https://github.com/NixOS/nixpkgs/blob/e6ab46982debeab9831236869539a507f670a129/pkgs/os-specific/linux/wooting-udev-rules/default.nix#L18>.
