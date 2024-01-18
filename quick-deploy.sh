31ea8fa7b4ad01c8e207d54ad47a1c9e9f2bc26e
Jason Yundt <jason@jasonyundt.email>
2024-01-18 11:32:01 -0500

    Fix contribution year for quick-deploy.sh
    427629a (Don’t leave result symlink for quick-deploy.sh, 2024-01-18)
    should have bumped the year already, but I forgot to do so.

--- quick-deploy.sh
+++ quick-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash -p nix
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
-# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2023)
+# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2023–2024)
 set -e
 # See sysexits.h.
 readonly ex_usage=64
