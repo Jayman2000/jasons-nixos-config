2a8ca2c57411eef17430475e59a361f3cf1fae87
Jason Yundt <jason@jasonyundt.email>
2024-01-18 11:44:51 -0500

    Make quick-deploy.sh prompt for password faster
    Before this commit, quick-deploy.sh used this shebang:
    
    	#!/usr/bin/env nix-shell
    	#! nix-shell -i bash -p nix
    
    Unfortunately, using nix-shell as an interpreter [1] is slow. Before
    this commit, it would take a second or two for nix-shell to start. As a
    result, the user would have to wait a second or two before entering
    their password. This change will (hopefully) eliminate that short delay.
    
    [1]: <https://nixos.org/manual/nix/stable/command-ref/nix-shell#use-as-a--interpreter>

--- quick-deploy.sh
+++ quick-deploy.sh
@@ -1,16 +1,18 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -i bash -p nix
+#!/usr/bin/env bash
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
 # SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2023–2024)
 set -e
 # See sysexits.h.
 readonly ex_usage=64
 
-if ! type sudo &> /dev/null
-then
-	echo "ERROR: the sudo command isn’t available." 1>&2
-	exit 1
-fi
+for dependency in sudo nix-shell
+do
+	if ! type "$dependency" &> /dev/null
+	then
+		echo "ERROR: the $dependency command isn’t available." 1>&2
+		exit 1
+	fi
+done
 
 if [ "$#" -eq 1 ]
 then
