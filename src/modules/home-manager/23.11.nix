7da34529cf4b2c0a2f8ddb2bc9ef736a99305229
Jason Yundt <jason@jasonyundt.email>
2024-02-04 09:53:28 -0500

    Add customLib.fetchFromGitHubNoHash
    The motivation behind this commit is to make code repeat itself less.
    Additionally, a future commit is going to add another tarball that gets
    fetched from GitHub. This commit will make it easier for me to write
    that future commit because I won’t have to go back an look at how to
    properly format GitHub URLs.

--- src/modules/home-manager/23.11.nix
+++ src/modules/home-manager/23.11.nix
@@ -1,10 +1,17 @@
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
-# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022–2023)
+# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022–2024)
 # This essentially does nix-channel --add "<url>" home-manager
 # Sources:
 # <https://nix-community.github.io/home-manager/index.html#sec-install-nixos-module>
 # <https://nixos.wiki/wiki/Home_Manager>
-let
-	home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz";
-in
-import "${home-manager}/nixos"
+{ lib, ... }:
+{
+	imports = let
+		customLib = import ../../lib.nix { inherit lib; };
+		home-manager = customLib.fetchFromGitHubNoHash {
+			owner = "nix-community";
+			repo = "home-manager";
+			rev = "release-23.11";
+		};
+	in [ "${home-manager}/nixos" ];
+}
--- src/modules/nixpkgs-unstable.nix
+++ src/modules/nixpkgs-unstable.nix
@@ -1,6 +1,15 @@
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
-# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2023)
+# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2024)
+{
+	pkgs ? import <nixpkgs> { },
+	lib ? pkgs.lib,
+	customLib ? import ../lib.nix { inherit lib; }
+}:
 let
-	nixpkgsUnstableTarball = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-unstable.tar.gz";
+	nixpkgsUnstableTarball = customLib.fetchFromGitHubNoHash {
+		owner = "NixOS";
+		repo = "nixpkgs";
+		rev = "nixos-unstable";
+	};
 in
 import nixpkgsUnstableTarball {}
