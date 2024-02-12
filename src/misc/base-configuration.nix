2cf926877765e895a82f4fd38913d6e038b54f1e
Jason Yundt <jason@jasonyundt.email>
2024-02-12 10:03:55 -0500

    Make configuration.nix files less repetitious
    A future commit will leverage the change made in this commit in order to
    make machine’s aware of their machine slugs.

--- src/misc/base-configuration.nix
+++ src/misc/base-configuration.nix
@@ -1,3 +1,13 @@
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
 # SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2024)
-import ../../../misc/base-configuration.nix "graphical-test-vm"
+jncMachineSlug:
+{ config, ... }:
+{
+	imports = let
+		modulePath = dirName:
+			"${../.}/modules/${dirName}/${jncMachineSlug}.nix";
+	in [
+		(modulePath "")
+		(modulePath "hardware-configuration.nix")
+	];
+}
--- src/modules/configuration.nix/jasonyundt.website.home.arpa/default.nix
+++ src/modules/configuration.nix/jasonyundt.website.home.arpa/default.nix
@@ -1,10 +1,3 @@
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
 # SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2024)
-let
-	file = "/jasonyundt.website.home.arpa.nix";
-in {
-	imports = [
-		(../../hardware-configuration.nix + file)
-		(../.. + file)
-	];
-}
+import ../../../misc/base-configuration.nix "jasonyundt.website.home.arpa"
