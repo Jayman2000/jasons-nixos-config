0f25088eab445f2290d2b55cba98e65be84423ee
Jason Yundt <jason@jasonyundt.email>
2024-02-19 16:04:44 -0500

    Remove tmpfs-size-test
    That script only existed in order to test out an old commit. Now that
    that old commit has been tested, I no longer need tmpfs-size-test. I
    probably haven’t run that script in over a year. I don’t even know if it
    still works.
    
    I’m working on a future commit that’s going to modify every Bash script
    and every Nix expression that contains Bash code. The main motivation
    behind this commit is to make testing that future commit easier. That
    future commit might break tmpfs-size-test. Instead of bothering to test
    and potentially fix tmpfs-size-test, I’m choosing to opt out of
    maintaining it altogether.

--- tmpfs-size-test.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p nix
-# SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
-# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022–2023)
-
-nix-build ./tmpfs-size-test.nix && ./result "$@"
