89c3ac0c5b0d87628c7bc6a6a02c02c4daec194a
Jason Yundt <jason@jasonyundt.email>
2024-03-08 07:33:30 -0500

    Remove unnecessary arguments from vm-host.nix
    Those arguments weren’t being used for anything.

--- src/modules/vm-host.nix
+++ src/modules/vm-host.nix
@@ -1,6 +1,5 @@
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
-# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022)
-{ config, pkgs, ... }:
+# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2022, 2024)
 {
 	virtualisation.libvirtd.enable = true;
 	programs.dconf.enable = true;
