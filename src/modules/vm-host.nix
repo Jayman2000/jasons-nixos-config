f0e3e0c92008ebf428b421e2764292eb189670e4
Jason Yundt <jason@jasonyundt.email>
2024-03-08 09:55:52 -0500

    Automatically start default network for VMs
    Having to run “sudo virsh net-start default” every time I booted was
    getting annoying. Technically, I could have just run
    “sudo virsh net-autostart default”, but I wanted to make this change
    declaratively so that I don’t have to remember to do it every time I
    reinstall NixOS.

--- src/modules/vm-host.nix
+++ src/modules/vm-host.nix
@@ -4,18 +4,27 @@
 {
 	virtualisation.libvirtd.enable = true;
 	programs.virt-manager.enable = true;
-	# This next part makes sure that VMs created on Graphical-Test-VM can
-	# access the Internet. Without this next part, libvirt would use the
-	# default network configuration. The default network configuration gives
-	# VMs IP addresses that start with 192.168.122. Graphical-Test-VM is a
-	# VM, so its own IP address starts with 192.168.122. As a result, it
-	# can’t give VMs IP addresses that start with 192.168.122 because that
-	# range is already being used by its own network adapter.
 	systemd.tmpfiles.settings."libvirt-default-network-files" = let
 		networksDir = "/var/lib/libvirt/qemu/networks";
 		defaultNetworkFile = "${networksDir}/default.xml";
+		defaultNetworkAutostartFile = "${networksDir}/autostart/default.xml";
 		isSystemVM = config.jnc.machineSlug == "graphical-test-vm";
-	in lib.attrsets.optionalAttrs isSystemVM {
+	in {
+		# This makes it so that I don’t have to run
+		# “sudo virsh net-start default” every time I reboot. Credit
+		# goes to @amarshall (https://github.com/amarshall) for this
+		# idea:
+		# <https://github.com/NixOS/nixpkgs/issues/73418#issuecomment-1676439600>
+		"${defaultNetworkAutostartFile}"."L+".argument = defaultNetworkFile;
+	} // lib.attrsets.optionalAttrs isSystemVM {
+		# This part makes sure that VMs created on Graphical-Test-VM can
+		# access the Internet. Without this next part, libvirt would use
+		# the default network configuration. The default network
+		# configuration gives VMs IP addresses that start with
+		# 192.168.122. Graphical-Test-VM is a VM, so its own IP address
+		# starts with 192.168.122. As a result, it can’t give VMs IP
+		# addresses that start with 192.168.122 because that range is
+		# already being used by its own network adapter.
 		"${defaultNetworkFile}"."L+".argument = let
 			libvirtNetworkForBareMetal = "${pkgs.libvirt}${defaultNetworkFile}";
 			libvirtNetworkForGuest = let
