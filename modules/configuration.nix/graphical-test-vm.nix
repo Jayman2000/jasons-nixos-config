400a40d5d980061deaf232ca46829fd82869ecaf
Jason Yundt <jason@jasonyundt.email>
2024-09-13 08:02:17 -0400

    Specify filesystems for Graphical-Test-VM
    Before this change, if you ran this command,
    
        nixos-rebuild --flake .#graphicalTestVM build
    
    then it would give you errors. This commit fixes one of those errors.

--- modules/configuration.nix/graphical-test-vm.nix
+++ modules/configuration.nix/graphical-test-vm.nix
@@ -1,4 +1,15 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: 2024 Jason Yundt <jason@jasonyundt.email>
 {
+    fileSystems = {
+        "/" = {
+            device = "UUID=c304403c-6ce0-4157-a079-2cbb6fb01b9c";
+            fsType = "bcachefs";
+        };
+        "/boot" = {
+            device = "/dev/disk/by-uuid/1358-F1E8";
+            fsType = "vfat";
+            options = [ "fmask=0022" "dmask=0022" ];
+        };
+    };
 }
