041761517d8f242dffb21a8fc0afeb72687b6e10
Jason Yundt <jason@jasonyundt.email>
2024-09-13 05:34:50 -0400

    Create flake
    I’m using a flake for my NixOS config for two reasons:
    
    1. Using flakes makes it easier to pin Nixpkgs (and anything else that I
    end up using).
    
    2. Using flakes for my NixOS config will cause me to learn more about
    flakes. Learning more about flakes is valuable because I sometimes
    contribute to projects that use flakes. Additionally, I think that I’ll
    probably encounter problems in the future and think “hey, I can use this
    thing that I learned about flakes to help me solve this problem.”

---
+++ flake.nix
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2024 Jason Yundt <jason@jasonyundt.email>
+{
+    description = "The flake for Jason’s NixOS Config";
+    outputs = { self }: { };
+}
