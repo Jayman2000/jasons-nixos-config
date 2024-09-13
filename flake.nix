162778fea5a94ddc5cb44f6294ed5cca0d79a52d
Jason Yundt <jason@jasonyundt.email>
2024-09-13 05:49:18 -0400

    Enable pure evaluation mode
    In the previous commit message, I mentioned I decided to use flakes for
    multiple reasons. One of those reasons was that flakes make it easier to
    pin the versions of any repos that I’m using. Enabling pure evaluation
    mode helps me achieve my goal of pinning the versions of any repos that
    I use.

--- flake.nix
+++ flake.nix
@@ -2,5 +2,7 @@
 # SPDX-FileCopyrightText: 2024 Jason Yundt <jason@jasonyundt.email>
 {
     description = "The flake for Jason’s NixOS Config";
+    nixConfig.pure-eval = true;
+
     outputs = { self }: { };
 }
