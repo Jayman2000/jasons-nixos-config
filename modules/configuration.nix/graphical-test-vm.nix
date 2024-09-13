1dd119b1d121504f6aa5eea487826d731f4a7f32
Jason Yundt <jason@jasonyundt.email>
2024-09-13 08:06:56 -0400

    Give Graphical-Test-VM a bootloader
    Before this change, if you ran this command,
    
        nixos-rebuild --flake .#graphicalTestVM build
    
    then it would give you an error. This commit fixes that error.

--- modules/configuration.nix/graphical-test-vm.nix
+++ modules/configuration.nix/graphical-test-vm.nix
@@ -1,6 +1,10 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: 2024 Jason Yundt <jason@jasonyundt.email>
 {
+    boot.loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+    };
     fileSystems = {
         "/" = {
             device = "UUID=c304403c-6ce0-4157-a079-2cbb6fb01b9c";
