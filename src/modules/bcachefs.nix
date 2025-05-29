3433baa2d344e0bf91b4f2cf7b2ee890c9d7ce9d
Jason Yundt <jason@jasonyundt.email>
2025-05-29 13:23:08 -0400

    Lock Linux kernel version
    Before this change, the configs in this repository used
    linuxPackages_latest. At the moment, linuxPackages_latest provides Linux
    kernel version 6.14.8. Soon, I’m going to try to upgrade from NixOS
    24.11 to NixOS 25.05. In NixOS 25.05, linuxPackages_latest provides a
    newer version of the Linux kernel. Sometimes, switching between versions
    of the Linux kernel can take a very long time on my systems due to
    bcachefs-related changes. Sometimes, I end up having to boot into a USB
    drive and manually doing an fsck. I don’t want to have to do that when
    switching between versions of NixOS so I’m locking the version now. I’ll
    unlock the version after I’ve finished upgrading all of the configs in
    this repository to NixOS 25.05.
    
    I chose to use linuxPackages_6_12 because on NixOS 25.05 the default
    version of the Linux kernel is 6.12 at the moment. This means that I’ll
    be able to completely get rid of the boot.kernelPackages part of the
    configs after I’ve finished upgrading to NixOS 25.05.

--- src/modules/bcachefs.nix
+++ src/modules/bcachefs.nix
@@ -1,5 +1,5 @@
 # SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
-# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2024)
+# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2024–2025)
 { pkgs, lib, ... }:
 {
 	boot.supportedFilesystems = [ "bcachefs" ];
@@ -12,7 +12,7 @@
 	#
 	# [1]: <https://web.archive.org/web/20240313213107/https://search.nixos.org/packages?channel=23.11&show=linux&from=0&size=50&sort=relevance&type=packages&query=linux>
 	# [2]: <https://lore.kernel.org/lkml/CAHk-=widprp4XoHUcsDe7e16YZjLYJWra-dK0hE1MnfPMf6C3Q@mail.gmail.com/>
-	boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+	boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_12;
 	# Hopefully, this will make my system swap less excessively. See
 	# <https://old.reddit.com/r/bcachefs/comments/1d76l99/using_bcachefs_made_my_system_swap_too_much_but_i/>.
 	boot.kernel.sysctl."vm.swappiness" = 1;
