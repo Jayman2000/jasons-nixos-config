062feccbe0989d73b38adec332c034fb0dabd3ca
Jason Yundt <jason@jasonyundt.email>
2025-06-02 11:36:49 -0400

    Give flake a description
    The main motivation behind this change is to make it easier to create a
    future commit. Specifically, that future commit will make the flake
    comply with Jason’s Nix Flake Style Guide [1]. In order to comply with
    Jason’s Nix Flake Style Guide, flakes need to have descriptions.
    
    [1]: <https://codeberg.org/JasonYundt/jasons-nix-flake-style-guide>

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: 2025 Jason Yundt <jason@jasonyundt.email>
 {
+  description = "Tools for deploying NixOS the way that I do";
   outputs = inputs: { };
 }
