6fac4c99b460a4ff6979232b4ed03af8349a8182
Jason Yundt <jason@jasonyundt.email>
2025-06-02 09:25:18 -0400

    Restart from scratch
    I have changed a lot since I started the previous major version of my
    NixOS configs. I know a lot more about the Nix ecosystem, and I am much
    more skilled at using it. If I had created this repository today, then I
    would do a lot of things differently. Additionally, this repository has
    accumulated a lot of technical debt. Restarting this repository from
    scratch will give me a chance to improve pretty much every aspect of my
    configs and will give me a chance to get rid of all of the technical
    debt.
    
    This commit is dedicated to the public domain using 🅭🄍1.0 [1].
    
    [1]: <https://creativecommons.org/publicdomain/zero/1.0>

--- src/modules/wooting.nix
+++
@@ -1,8 +0,0 @@
-# SPDX-FileNotice: 🅭🄍1.0 This file is dedicated to the public domain using the CC0 1.0 Universal Public Domain Dedication <https://creativecommons.org/publicdomain/zero/1.0/>.
-# SPDX-FileContributor: Jason Yundt <jason@jasonyundt.email> (2023)
-{
-	hardware.wooting.enable = true;
-	# This is needed for the udev rules to work. See
-	# <https://github.com/NixOS/nixpkgs/blob/e6ab46982debeab9831236869539a507f670a129/pkgs/os-specific/linux/wooting-udev-rules/default.nix#L18>.
-	users.users.jayman.extraGroups = [ "input" ];
-}
