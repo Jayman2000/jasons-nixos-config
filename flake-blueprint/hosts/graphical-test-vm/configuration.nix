41ec475637f77c1fa5a4d1cb4ef2ffd2c54647d2
Jason Yundt <jason@jasonyundt.email>
2025-06-04 16:23:31 -0400

    Pin kernel version
    I need to do this in order to make sure that Jason-Desktop-Linux
    doesn’t end up taking a really long time to boot.

--- flake-blueprint/hosts/graphical-test-vm/configuration.nix
+++ flake-blueprint/hosts/graphical-test-vm/configuration.nix
@@ -1,6 +1,13 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: 2025 Jason Yundt <jason@jasonyundt.email>
-{ inputs, modulesPath, perSystem, ... }:
+{
+  inputs,
+  lib,
+  modulesPath,
+  perSystem,
+  pkgs,
+  ...
+}:
 {
   system.stateVersion = "25.05";
   nixpkgs.hostPlatform = "x86_64-linux";
@@ -10,6 +17,39 @@
     (modulesPath + "/profiles/qemu-guest.nix")
     inputs.disko.nixosModules.default
   ];
+
+  # I use bcachefs on Jason-Desktop-Linux. Bcachefs was added to Linux
+  # in Linux version 6.7 [1]. This means that I need to use Linux
+  # version 6.7 or greater.
+  #
+  # Unfortunately, I can’t just use any kernel version that’s greater
+  # than or equal to 6.7. Specifically, I tried using Linux 6.12.x, and
+  # it made Jason-Desktop-Linux take over five hours to boot. I want to
+  # switch back to NixOS’s default Linux kernel version, but I can’t
+  # until the default Linux kernel version is greater than or equal to
+  # 6.14 (I haven’t done extensive testing, but I believe that 6.14 is
+  # the first version of Linux where my desktop will take less than five
+  # hours to boot).
+  #
+  # With all of that in mind, here’s the process that should be followed
+  # whenever the Linux kernel version is changed:
+  #
+  # 1. Check to see if NixOS’s default version of Linux is greater than
+  # or equal to 6.14. If it is, then make this next line use the default
+  # version of Linux.
+  #
+  # 2. Check <https://www.kernel.org/category/releases.html> to see if
+  # there’s any LTS kernels with version numbers greater than or equal
+  # to 6.14. If there are, then use the LTS kernel with the lowest
+  # version number that’s greater than or equal to 6.14.
+  #
+  # 3. Otherwise, use the kernel that’s in Nixpkgs that has the lowest
+  # version number that’s greater than or equal to 6.14.
+  #
+  # editorconfig-checker-disable
+  # [1]: <https://lore.kernel.org/lkml/CAHk-=widprp4XoHUcsDe7e16YZjLYJWra-dK0hE1MnfPMf6C3Q@mail.gmail.com/>
+  # editorconfig-checker-enable
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_14;
 
   disko.devices.disk.main = {
     device = "/dev/disk/by-path/pci-0000:04:00.0";
