7c2b5b6a7b6e293190631ff2fb3bcf37d17480aa
Jason Yundt <jason@jasonyundt.email>
2025-07-01 11:13:19 +0000

    Make copy and pasting work in Neovim
    There are a few different packages that I could have installed in order
    to make copy and pasting work. I chose wl-clipboard-rs because it’s
    native to Wayland and it’s written in Rust. The other Wayland copy and
    paste tools that I saw were written in the C Programming Language. If I
    ever run into a problem, then it’s going to be much easier for me to
    contribute to a project written in Rust than it would be for me to
    contribute to a project written in C.

--- flake-blueprint/modules/nixos/neovim.nix
+++ flake-blueprint/modules/nixos/neovim.nix
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: 2025 Jason Yundt <jason@jasonyundt.email>
+{ pkgs, ... }:
 {
+  environment.systemPackages = [ pkgs.wl-clipboard-rs ];
   programs.neovim = {
     enable = true;
     defaultEditor = true;
