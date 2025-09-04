e6e91961a0534abcaf2d7edc9cc0e681619364e7
Jason Yundt <jason@jasonyundt.email>
2025-09-04 14:59:02 -0400

    Make jayman a trusted-user
    Sometimes, I run the script from this post [1] in order to work around
    certain build failures. In order for the script from that post to work,
    your user account needs to be a trusted user.
    
    [1]: <https://github.com/NixOS/nixpkgs/issues/430002#issuecomment-3218541887>

--- flake-blueprint/modules/nixos/nix.nix
+++ flake-blueprint/modules/nixos/nix.nix
@@ -3,11 +3,19 @@
 /**
   Configuration for the Nix package manager.
 */
-{ perSystem, ... }:
+{
+  config,
+  lib,
+  perSystem,
+  ...
+}:
 {
   nix = {
     package = perSystem.self.nix;
     settings = {
+      trusted-users = lib.modules.mkIf (lib.attrsets.hasAttr "jayman" config.users.users) [
+        config.users.users.jayman.name
+      ];
       use-xdg-base-directories = true;
       # This is supposed to be one gibibyte in bytes.
       download-buffer-size = 1024 * 1024 * 1024;
