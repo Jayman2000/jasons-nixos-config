d2b10db753138e11b27e009fa7ddcb489790d7c9
Jason Yundt <jason@jasonyundt.email>
2025-11-05 07:43:26 -0500

    Stop pinning the Linux kernel version
    The version of NixOS’s bcachefs module that’s in newer revisions of
    Nixpkgs’s nixos-25.05 branch will automatically set the kernel version
    for you [1]. It will probably be better to follow the version that the
    module uses than to use our own version.
    
    Additionally, the version of NixOS’s bcachefs module that’s in newer
    revisions of Nixpkgs’s nixos-25.05 branch use the bcachefs DKMS module
    rather than the built-in kernel module [2]. This means that pinning the
    kernel version will not prevent bcachefs upgrades. The only reason why I
    was pinning the kernel version was to delay bcachefs upgrades.
    
    This change is being made in order to prepare for a future commit. In
    that future commit, we will start using a newer revision of Nixpkgs’s
    nixos-25.05 branch.
    
    [1]: <https://github.com/NixOS/nixpkgs/blob/09d3831f44af7689a2bc1b190ee69160561da363/nixos/modules/tasks/filesystems/bcachefs.nix#L246>
    [2]: <https://discourse.nixos.org/t/bcachefs-with-future-kernel-versions/71713>
    
    (cherry picked from commit c2cf0d3635761fa9fd00d9121980a9c3ab691fb8)

--- src/modules/bcachefs.nix
+++ src/modules/bcachefs.nix
@@ -3,40 +3,6 @@
 { pkgs, lib, ... }:
 {
 	boot.supportedFilesystems = [ "bcachefs" ];
-	# At the moment, pkgs.linux gives you Linux 6.6.92 [1]. Bcachefs didn’t
-	# make it into the kernel until Linux 6.7 [2]. That’s why we have to
-	# tell it to use a newer kernel here. Also, we have to use mkForce here
-	# because src/modules/installation-image.nix imports another module
-	# that has a conflicting value for
-	# boot.kernelPackages.
-	#
-	# Unfortunately, I can’t just use any kernel version that’s greater
-	# than or equal to 6.7. Specifically, I tried using Linux 6.12.x, and
-	# it made Jason-Desktop-Linux take over five hours to boot. I want to
-	# switch back to NixOS’s default Linux kernel version, but I can’t
-	# until the default Linux kernel version is greater than or equal to
-	# 6.14 (I haven’t done extensive testing, but I believe that 6.14 is
-	# the first version of Linux where my desktop will take less than five
-	# hours to boot).
-	#
-	# With all of that in mind, here’s the process that should be followed
-	# whenever the Linux kernel version is changed:
-	#
-	# 1. Check to see if NixOS’s default version of Linux is greater than
-	# or equal to 6.14. If it is, then make this next line use the default
-	# version of Linux.
-	#
-	# 2. Check <https://www.kernel.org/category/releases.html> to see if
-	# there’s any LTS kernels with version numbers greater than or equal to
-	# 6.14. If there are, then use the LTS kernel with the lowest version
-	# number that’s greater than or equal to 6.14.
-	#
-	# 3. Otherwise, use the kernel that’s in Nixpkgs that has the lowest
-	# version number that’s greater than or equal to 6.14.
-	#
-	# [1]: <https://web.archive.org/web/20250531104125/https://search.nixos.org/packages?channel=24.11&show=linux&from=0&size=50&sort=relevance&type=packages&query=linux>
-	# [2]: <https://lore.kernel.org/lkml/CAHk-=widprp4XoHUcsDe7e16YZjLYJWra-dK0hE1MnfPMf6C3Q@mail.gmail.com/>
-	boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_17;
 	# Hopefully, this will make my system swap less excessively. See
 	# <https://old.reddit.com/r/bcachefs/comments/1d76l99/using_bcachefs_made_my_system_swap_too_much_but_i/>.
 	boot.kernel.sysctl."vm.swappiness" = 1;
