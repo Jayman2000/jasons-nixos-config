e04f13ec05126f1ce141255e6c01acf78532cdde
Jason Yundt <jason@jasonyundt.email>
2026-01-09 10:06:27 -0500

    Fix wrong formatter being used
    The previous commit updated flake.lock. It seems that that flake.lock
    update inadvertently caused “nix fmt” to use nixfmt-classic instead of
    nixfmt-rfc-style. This change fixes that issue.

--- flake-blueprint/formatter.nix
+++ flake-blueprint/formatter.nix
@@ -1,12 +1,15 @@
 # SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: 2025 Jason Yundt <jason@jasonyundt.email>
+# SPDX-FileCopyrightText: 2025–2026 Jason Yundt <jason@jasonyundt.email>
 { inputs, pkgs }:
 let
   inherit (inputs.treefmt-nix.lib) evalModule;
   treefmt-nixModule = {
     projectRootFile = "flake.nix";
     programs = {
-      nixfmt.enable = true;
+      nixfmt = {
+        enable = true;
+        package = pkgs.nixfmt-rfc-style;
+      };
       rustfmt = {
         enable = true;
         edition = "2024";
