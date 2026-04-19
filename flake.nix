7bdbd94f99b1fee180b4491025373614ebf5bb11
Jason Yundt <jason@jasonyundt.email>
2026-04-19 17:01:56 -0400

    Rename “nixpkgs” directory to “nixpkgs-25.05”
    The main motivation behind this change is to make it easier to create a
    future change. The future change will add a second nixpkgs subtree. The
    second nixpkgs subree will be put in a directory that’s named
    “nixpkgs-25.11”.

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: 2025 Jason Yundt <jason@jasonyundt.email>
+# SPDX-FileCopyrightText: 2025–2026 Jason Yundt <jason@jasonyundt.email>
 {
   description = "Tools for deploying NixOS the way that I do";
 
   inputs = {
-    nixpkgs.url = "path:./nixpkgs";
+    nixpkgs.url = "path:./nixpkgs-25.05";
     blueprint = {
       url = "github:numtide/blueprint";
       inputs = {
