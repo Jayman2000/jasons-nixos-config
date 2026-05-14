9cfc6cf8d3339061a0d7fe88cb8d950b0f715a7a
Jason Yundt <jason@jasonyundt.email>
2026-05-14 10:53:16 -0400

    Stop Jason-Desktop-Linux from being its own remote builder
    Before this change, Jason-Desktop-Linux would try to use itself as a
    remote builder. Doing that is pointless and inefficient. This change
    fixes that issue.
    
    This change fixes the issue by introducing a new function named
    mkBuildMachineModule. At the moment, having a mkBuildMachineModule is
    kind of pointless. It just adds complexity without much benefit. It was
    added in order to make it easier to create a future commit. Those future
    commits will make it so that Jason-Lemur-Pro can also be used as a
    remote builder.

--- flake-blueprint/modules/nixos/nix.nix
+++ flake-blueprint/modules/nixos/nix.nix
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: 2025 Jason Yundt <jason@jasonyundt.email>
+# SPDX-FileCopyrightText: 2025–2026 Jason Yundt <jason@jasonyundt.email>
 /**
   Configuration for the Nix package manager.
 */
@@ -9,39 +9,53 @@
   perSystem,
   ...
 }:
-{
-  nix = {
-    package = perSystem.self.nix;
+let
+  unconditionalConfig = {
+    nix = {
+      package = perSystem.self.nix;
 
-    # The main goal of using a flake for my NixOS config is to make things more
-    # reproducible. Channels lead to less reproducibility so I’m disabling
-    # them.
-    channel.enable = false;
+      # The main goal of using a flake for my NixOS config is to make things more
+      # reproducible. Channels lead to less reproducibility so I’m disabling
+      # them.
+      channel.enable = false;
 
-    distributedBuilds = true;
-    buildMachines = [
-      {
-        hostName = "Jason-Desktop-Linux";
-        maxJobs = 12;
-        sshUser = "jayman";
-        supportedFeatures = [ "big-parallel" ];
-        systems = [
-          "i686-linux"
-          "x86_64-linux"
+      distributedBuilds = true;
+
+      settings = {
+        trusted-users = lib.modules.mkIf (lib.attrsets.hasAttr "jayman" config.users.users) [
+          config.users.users.jayman.name
         ];
-      }
-    ];
-
-    settings = {
-      trusted-users = lib.modules.mkIf (lib.attrsets.hasAttr "jayman" config.users.users) [
-        config.users.users.jayman.name
-      ];
-      use-xdg-base-directories = true;
-      # This is supposed to be one gibibyte in bytes.
-      download-buffer-size = 1024 * 1024 * 1024;
-      # This next setting results in better error messages and less builds that
-      # get canceled partway through.
-      keep-going = true;
+        use-xdg-base-directories = true;
+        # This is supposed to be one gibibyte in bytes.
+        download-buffer-size = 1024 * 1024 * 1024;
+        # This next setting results in better error messages and less builds that
+        # get canceled partway through.
+        keep-going = true;
+      };
     };
   };
+  mkBuildMachineModule =
+    { hostName, maxJobs }:
+    {
+      nix.buildMachines = lib.modules.mkIf (config.networking.hostName != hostName) [
+        {
+          inherit hostName maxJobs;
+          sshUser = "jayman";
+          supportedFeatures = [ "big-parallel" ];
+          systems = [
+            "i686-linux"
+            "x86_64-linux"
+          ];
+        }
+      ];
+    };
+in
+{
+  config = lib.modules.mkMerge [
+    unconditionalConfig
+    (mkBuildMachineModule {
+      hostName = "Jason-Desktop-Linux";
+      maxJobs = 12;
+    })
+  ];
 }
