eafbf80f5c4565cb8df469716a72203ed9399f75
Jason Yundt <jason@jasonyundt.email>
2026-05-14 13:00:32 -0400

    Allow Graphical-Test-VM to use other machines as Nix remote builders

--- flake-blueprint/modules/nixos/ssh.nix
+++ flake-blueprint/modules/nixos/ssh.nix
@@ -21,6 +21,7 @@
   users.users.jayman.openssh.authorizedKeys.keys = [
     # editorconfig-checker-disable
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINtGh8BdxRt7Pu3J82SIjzOWRCnuqpHpzYSmlMcCXaBO jayman@Graphical-Test-VM"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILC3F4rFOhk7jKm5/0S4KnJma+gz3vM6Cn3Vo8TTY3CA root@Graphical-Test-VM"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWQkgb4A4mvzHeXAm6ghxfknl15cttipb56qP0IpBlj jayman@Jason-Desktop-Linux"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIClHCYdk1dsNMnTm4J2saSiy/7YkK3KKr0S5TfJqynYv root@Jason-Desktop-Linux"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP5Zve4N1Oxbsx0J7a85U3jtiPgR+di3nX094KLiTgdn jayman@Jason-Lemur-Pro"
