9853126d2f85f7c3374df6051473d32f0e120878
Jason Yundt <jason@jasonyundt.email>
2026-05-14 08:08:37 -0400

    Update SSH key for jayman@Graphical-Test-VM
    Before this change, the SSH key for jayman@Graphical-Test-VM was
    inaccurate. This change fixes that issue.

--- flake-blueprint/modules/nixos/ssh.nix
+++ flake-blueprint/modules/nixos/ssh.nix
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: 2025 Jason Yundt <jason@jasonyundt.email>
+# SPDX-FileCopyrightText: 2025–2026 Jason Yundt <jason@jasonyundt.email>
 {
   services.openssh = {
     enable = true;
@@ -15,7 +15,7 @@
   };
   users.users.jayman.openssh.authorizedKeys.keys = [
     # editorconfig-checker-disable
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFfXiVplYdjUgnYeldCI2RIMI8afUvM3XnJ8IRZnHnGz jayman@Graphical-Test-VM"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINtGh8BdxRt7Pu3J82SIjzOWRCnuqpHpzYSmlMcCXaBO jayman@Graphical-Test-VM"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWQkgb4A4mvzHeXAm6ghxfknl15cttipb56qP0IpBlj jayman@Jason-Desktop-Linux"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGwzCr7V2+t3NBrwztLtRbkeGeb1Fps6jU69E3g7g4OI jayman@Jason-Lemur-Pro"
     # editorconfig-checker-enable
