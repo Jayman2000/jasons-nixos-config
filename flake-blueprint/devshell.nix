321e93589507fd52f434a1c5b7620e07e84a0931
Jason Yundt <jason@jasonyundt.email>
2026-05-16 13:58:34 -0400

    Remove code that isn’t necessary anymore
    a6cc535388a6f7bc617e80d341fe51cdf2137afb should have removed this code,
    but it didn’t. Oops.

--- flake-blueprint/devshell.nix
+++ flake-blueprint/devshell.nix
@@ -25,21 +25,6 @@ pkgs.mkShell {
             "run0"
             "--setenv=NIX_CONFIG"
             "--"
-            # This next part is needed or else you might get this error
-            # when running nr:
-            #
-            # > evaluation warning: The pinned version of Nix may not
-            # >                     have been used to evaluate this
-            # >                     flake.
-            #
-            # TODO: After we switch to a version of Nixpkgs that doesn’t
-            # have this bug [1], this next part should be changed so
-            # that we use run0’s --setenv option instead of using the
-            # env command.
-            #
-            # [1]: <https://github.com/NixOS/nixpkgs/issues/420570>
-            "env"
-            $"PATH=($env.PATH | str join ":")"
           ]
         }
         $command ++= [
