dda395e585427c5c03a8671ebaa570801a4ee33d
Jason Yundt <jason@jasonyundt.email>
2026-05-26 13:13:13 -0400

    Switch to Home Manager’s release-26.05 branch
    Now that that branch exists [1], we can use it.
    
    [1]: <https://github.com/nix-community/home-manager/issues/9294#event-25933587435>

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     forge-view-preview = {
