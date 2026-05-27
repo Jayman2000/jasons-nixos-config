529a07dbeb2e5741fd7b24e8e0b82d9cb004f3ff
Jason Yundt <jason@jasonyundt.email>
2026-05-27 08:20:20 -0400

    Use remote builders as substituters
    This allows us to skip downloading dependencies when a package has
    already been built by a remote builder [1].
    
    [1]: <https://wiki.nixos.org/wiki/Distributed_build#Using_remote_builders_as_substituters>

--- flake-blueprint/modules/nixos/nix.nix
+++ flake-blueprint/modules/nixos/nix.nix
@@ -25,6 +25,7 @@ let
 in
 {
   config,
+  inputs,
   lib,
   perSystem,
   ...
@@ -61,6 +62,58 @@ in
       # This next setting results in better error messages and less builds that
       # get canceled partway through.
       keep-going = true;
+      # This makes it so that machines can copy Nix store paths from remote
+      # builders before building starts.
+      substituters =
+        let
+          buildMachineToSubstituterURI =
+            value:
+            inputs.jasons-nix-flake-style-guide.lib.buildURI-reference {
+              scheme = value.protocol;
+              userinfo = value.sshUser;
+              host = value.hostName;
+              # TODO: I don’t think that setting path should be necessary here.
+              # If I don’t set it, I get an error, but I don’t think that I
+              # should get an error if it is not set (after all, the
+              # buildURI-reference function does have a default value for
+              # path). I think that there’s a bug with buildURI-reference that
+              # should be fixed.
+              path = [ "" ];
+              # Normally, Nix will refuse to copy Nix store paths from a
+              # substituter unless the path is signed by a trusted key. That
+              # being said, it will copy store paths from a remote builder even
+              # if the path is unsigned. In this situation, I’m declaring that
+              # certain machines are both substituters and remote builders.
+              # This means that unsigned paths will get copied anyway even if
+              # Nix’s substitution process is trying to protect me from them.
+              #
+              # This next part disables Nix’s normal behavior for these
+              # substituters. It makes it so that Nix will indeed copy unsigned
+              # paths from these substituters. (Nix will still refuse to copy
+              # unsigned paths from other substituters like
+              # <https://hydra.nixos.org/> though).
+              #
+              # You might think that this decreases security, but I don’t
+              # really think so. After all, we’re already trusting unsigned
+              # paths from these same machines when they are used as remote
+              # builders. You could make the argument that we should not trust
+              # unsigned paths from remote builders, but I don’t agree with
+              # that either. SSH’s know hosts mechanism already helps us make
+              # sure that we are connected to the real Jason-Desktop-Linux or
+              # the real Jason-Lemur-Pro when we copy paths from them. Nix
+              # store path–signature verification would make it so that we
+              # double check that we truly are connected to the real
+              # Jason-Desktop-Linux or the real Jason-Lemur-Pro every time we
+              # copy paths from them. Double checking is good, but in this case
+              # it would only increase security negligibly. Additionally, it
+              # would mean that I would have to maintain another set of key
+              # pairs for Jason-Desktop-Linux and Jason-Lemur-Pro (in addition
+              # to their SSH keys). I don’t want to put effort into doing that
+              # if there’s going to be basically no gain.
+              query = "trusted=1";
+            };
+        in
+        lib.lists.map buildMachineToSubstituterURI config.nix.buildMachines;
     };
   };
 }
