e0e9ca5ab897f3b0da0f05bbc9cff83fae479e82
Jason Yundt <jason@jasonyundt.email>
2026-05-27 06:30:04 -0400

    Simplify flake-blueprint/modules/nixos/nix.nix
    I was going to make a completely different change to
    flake-blueprint/modules/nixos/nix.nix, but they way that it was written
    was bothering me. I’m making this change now so that I can make the
    completely different change in a future commit without having to
    tolerate the fact that I could have made these other improvements to
    that file.

--- flake-blueprint/modules/nixos/nix.nix
+++ flake-blueprint/modules/nixos/nix.nix
@@ -3,56 +3,34 @@
 /**
   Configuration for the Nix package manager.
 */
+let
+  mkBuildMachineModule =
+    { hostName, maxJobs }:
+    (
+      { config, lib, ... }:
+      {
+        nix.buildMachines = lib.modules.mkIf (config.networking.hostName != hostName) [
+          {
+            inherit hostName maxJobs;
+            sshUser = "jayman";
+            supportedFeatures = [ "big-parallel" ];
+            systems = [
+              "i686-linux"
+              "x86_64-linux"
+            ];
+          }
+        ];
+      }
+    );
+in
 {
   config,
   lib,
   perSystem,
   ...
 }:
-let
-  unconditionalConfig = {
-    nix = {
-      package = perSystem.self.nix;
-
-      # The main goal of using a flake for my NixOS config is to make things more
-      # reproducible. Channels lead to less reproducibility so I’m disabling
-      # them.
-      channel.enable = false;
-
-      distributedBuilds = true;
-
-      settings = {
-        trusted-users = lib.modules.mkIf (lib.attrsets.hasAttr "jayman" config.users.users) [
-          config.users.users.jayman.name
-        ];
-        use-xdg-base-directories = true;
-        # This is supposed to be one gibibyte in bytes.
-        download-buffer-size = 1024 * 1024 * 1024;
-        # This next setting results in better error messages and less builds that
-        # get canceled partway through.
-        keep-going = true;
-      };
-    };
-  };
-  mkBuildMachineModule =
-    { hostName, maxJobs }:
-    {
-      nix.buildMachines = lib.modules.mkIf (config.networking.hostName != hostName) [
-        {
-          inherit hostName maxJobs;
-          sshUser = "jayman";
-          supportedFeatures = [ "big-parallel" ];
-          systems = [
-            "i686-linux"
-            "x86_64-linux"
-          ];
-        }
-      ];
-    };
-in
 {
-  config = lib.modules.mkMerge [
-    unconditionalConfig
+  imports = [
     (mkBuildMachineModule {
       hostName = "Jason-Desktop-Linux";
       maxJobs = 12;
@@ -62,4 +40,27 @@ in
       maxJobs = 8;
     })
   ];
+
+  nix = {
+    package = perSystem.self.nix;
+
+    # The main goal of using a flake for my NixOS config is to make things more
+    # reproducible. Channels lead to less reproducibility so I’m disabling
+    # them.
+    channel.enable = false;
+
+    distributedBuilds = true;
+
+    settings = {
+      trusted-users = lib.modules.mkIf (lib.attrsets.hasAttr "jayman" config.users.users) [
+        config.users.users.jayman.name
+      ];
+      use-xdg-base-directories = true;
+      # This is supposed to be one gibibyte in bytes.
+      download-buffer-size = 1024 * 1024 * 1024;
+      # This next setting results in better error messages and less builds that
+      # get canceled partway through.
+      keep-going = true;
+    };
+  };
 }
