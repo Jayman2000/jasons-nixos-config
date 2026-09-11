03a90de55314fd6ca419367a899fecf22a008f87
Jason Yundt <jason@jasonyundt.email>
2026-09-11 07:23:36 -0400

    Make flake-parts’s nixpkgs-lib input follow the top-level nixpkgs input
    Most of this repository’s flake’s inputs have their dependencies
    overridden in order to avoid using multiple different versions of the
    same flake and to make sure that dependencies are kept up to date even
    if the upstream project isn’t updating their flake.lock file. Before
    this change, flake-parts was one of this repository’s flake’s inputs
    that did not have its dependencies overridden. The fact that
    flake-parts’s dependencies were not overridden was an oversight. This
    change fixes that oversight.

--- flake.nix
+++ flake.nix
@@ -52,7 +52,10 @@
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     nur = {
       url = "github:nix-community/NUR";
       inputs = {
